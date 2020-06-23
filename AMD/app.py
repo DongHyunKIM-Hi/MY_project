c8170a88df6700a91e9719981c0e5f7a89b63a66
DongHyunKIM-Hi <57423518+DongHyunKIM-Hi@users.noreply.github.com>
2020-06-24 00:21:14 +0900

    final
    make text cloud
    make keyword

--- AMD/app.py
+++ AMD/app.py
@@ -1,10 +1,10 @@
-from pymongo import MongoClient  # pymongo를 임포트 하기(패키지 인스톨 먼저 해야겠죠?)
+from pymongo import MongoClient 
 from flask import Flask, render_template, jsonify, request
 app = Flask(__name__)
 
 
 client = MongoClient('localhost', 27017)  # mongoDB는 27017 포트로 돌아갑니다.
-db = client.AMD  # 'dbsparta'라는 이름의 db를 만듭니다.
+db = client.AMD  
 
 # HTML을 주는 부분
 
@@ -17,27 +17,75 @@ def home():
 @app.route('/list_movie', methods=['GET'])
 def list_movie_get():
     want_year = request.args.get('want_year')
-    print(want_year)
-    # movies_list = list(db.movie.find({'Year':str(want_year)}))
     movies_list = list(db.movie.find({'Year': int(want_year)}, {'_id': False}))
     return jsonify({'result':'success', 'movies':movies_list})
 
 @app.route('/list_drama', methods=['GET'])
 def list_drama_get():
     want_year = request.args.get('want_year')
-    print(want_year)
-    # movies_list = list(db.movie.find({'Year':str(want_year)}))
     dramas_list = list(db.drama.find({'Year': int(want_year)}, {'_id': False}))
     return jsonify({'result':'success', 'dramas':dramas_list})
 
 @app.route('/list_music', methods=['GET'])
 def list_music_get():
     want_year = request.args.get('want_year')
-    print(want_year)
-    # movies_list = list(db.movie.find({'Year':str(want_year)}))
     musics_list = list(db.music.find({'Year': str(want_year)+"년"}, {'_id': False}))
     return jsonify({'result':'success', 'musics':musics_list})
 
+@app.route('/best_list', methods=['GET'])
+def best_list_get():
+    want_year = request.args.get('want_year')
+    best_music = list(db.music.find({'Year': str(want_year)+"년"}, {'_id': False}).sort('like',-1))
+    best_drama = list(db.drama.find({'Year': int(want_year)}, {'_id': False}).sort('like',-1))
+    best_movie = list(db.movie.find({'Year': int(want_year)}, {'_id': False}).sort('like',-1))
+    return jsonify({'result':'success', 'music':best_music, 'drama': best_drama, 'movie': best_movie})
+
+@app.route('/music/like', methods=['POST'])
+def music_like():
+    name_receive = request.form['name_give']
+    like = db.music.find_one({'name':name_receive})
+    new_like = like['like']+1
+    db.music.update_one({'name':name_receive},{'$set':{'like':new_like}})
+    return jsonify({'result': 'success'})
+
+@app.route('/drama/like', methods=['POST'])
+def drama_like():
+    name_receive = request.form['name_give']
+    like = db.drama.find_one({'name':name_receive})
+    new_like = like['like']+1
+    db.drama.update_one({'name':name_receive},{'$set':{'like':new_like}})
+    return jsonify({'result': 'success'})
+
+@app.route('/movie/like', methods=['POST'])
+def movie_like():
+    name_receive = request.form['name_give']
+    
+    like = db.movie.find_one({'name':name_receive})
+    
+    new_like = like['like']+1
+    db.movie.update_one({'name':name_receive},{'$set':{'like':new_like}})
+
+    return jsonify({'result': 'success'})
+
+@app.route('/make_keyword', methods=['GET'])
+def keyword_get():
+    want_year = request.args.get('want_year')
+    keyword_list = db.keyWord.find_one({'Year': int(want_year)}, {'_id': False})
+    return jsonify({'result':'success', 'keyword':keyword_list})
+
+@app.route('/input_keyword', methods=['POST'])
+def keyword_post():
+    key_receive = request.form['key_give']
+    print("확인1")
+    year_receive = request.form['year_give']
+    print("확인2")
+    key = db.keyWord.find_one({'Year':int(year_receive)})
+    print(key['keyword_list'])
+    doc= str(key['keyword_list'])+","+str(key_receive)
+    print(doc)
+    db.keyWord.update_one({'Year':int(year_receive)},{'$set':{'keyword_list':doc}})
+
+    return jsonify({'result': 'success'})
 
 if __name__ == '__main__':
     app.run('localhost', port=5000, debug=True)
--- AMD/test.py
+++ AMD/test.py
@@ -1,25 +1,12 @@
+#-*- coding: utf-8 -*- 
 from pymongo import MongoClient
 from bs4 import BeautifulSoup
 import requests
 from flask import Flask, render_template, jsonify, request
 app = Flask(__name__)
-count = 0
+
 
 client = MongoClient('localhost', 27017)
 db = client.AMD
-count=0
-make_list = list(db.movie.find())
-for a in make_list:
-    headers = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
-    url='https://search.naver.com/search.naver?sm=top_hty&fbm=1&ie=utf8&query=%EC%98%81%ED%99%94+'+a['name']
-    #반복문을 사용하여 2015년부터 2019년까지 반복하여 url을 바꿔서 실행
-    data = requests.get(url,headers=headers)
-
-    soup = BeautifulSoup(data.text, 'html.parser')
-   
-    movies = soup.select_one('#_au_movie_info > div.info_main > div > a > img')
-    img_url=movies.get('src')
-    count=count+1
-    print(count)
-    print(img_url)
-    db.movie.update_one({'name':a['name']},{'$set':{'img_url':img_url}})
+doc="세월호 세월호 겨울왕국 let it go 명량 가오갤 인터스텔라 하지마 별그대 김수현 전지현 정도전 장보리 미생 밀회 피노키오 AOA 짧은치마 단발머리 눈코입 연결고리 마마무 방탄 상남자 걸스데이 엘사 울라프 엘사 울라프 월드컵 월드컵 월드컵 월드컵 으리 신토부으리 의리 의리 의리 의리 김보성 호갱 흑우 호갱님 호갱 "
+db.keyWord.update_one({'Year':2014},{'$set':{'keyword_list':doc}})
